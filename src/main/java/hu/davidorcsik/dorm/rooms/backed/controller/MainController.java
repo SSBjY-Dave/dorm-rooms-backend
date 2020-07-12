cc3427c9b5c081def6961571c6ae176e57c7f630
David Orcsik <davidorcsik@protonmail.com>
2020-07-12 22:51:53 +0200

    Lazy fetching problems mitigated by direct query in getter

--- src/main/java/hu/davidorcsik/dorm/rooms/backed/controller/MainController.java
+++ src/main/java/hu/davidorcsik/dorm/rooms/backed/controller/MainController.java
@@ -3,7 +3,10 @@ package hu.davidorcsik.dorm.rooms.backed.controller;
 import hu.davidorcsik.dorm.rooms.backed.entity.People;
 import hu.davidorcsik.dorm.rooms.backed.model.PeopleModel;
 import hu.davidorcsik.dorm.rooms.backed.model.RoomModel;
+import hu.davidorcsik.dorm.rooms.backed.security.UserWrapper;
 import hu.davidorcsik.dorm.rooms.backed.types.ReservationData;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,8 @@ public class MainController {
     public String home() {
         return "home page";
     }
-//    @GetMapping("/test/")
-//    public ReservationData test() {
-//        return new ReservationData(PeopleModel.getInstance().getME(), RoomModel.getInstance().getById());
-//    }
+    @GetMapping("/test")
+    public People test() {
+        return ((UserWrapper)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getPeople();
+    }
 }
--- src/main/java/hu/davidorcsik/dorm/rooms/backed/security/UserWrapper.java
+++ src/main/java/hu/davidorcsik/dorm/rooms/backed/security/UserWrapper.java
@@ -1,7 +1,9 @@
 package hu.davidorcsik.dorm.rooms.backed.security;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import hu.davidorcsik.dorm.rooms.backed.entity.People;
 import hu.davidorcsik.dorm.rooms.backed.entity.Role;
+import hu.davidorcsik.dorm.rooms.backed.model.PeopleModel;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -9,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class UserWrapper implements UserDetails {
+    @JsonIgnore
     private final People people;
 
     public UserWrapper(People people) {
@@ -16,7 +19,7 @@ public class UserWrapper implements UserDetails {
     }
 
     public People getPeople() {
-        return people;
+        return PeopleModel.getInstance().getDatabaseEntity(people).orElse(null);
     }
 
     @Override
