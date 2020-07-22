7b0f88cb85d1eab48e86c40ee3f167a92e165a93
David Orcsik <davidorcsik@protonmail.com>
2020-07-22 22:11:45 +0200

    Excel demo on /test mapping

--- src/main/java/hu/davidorcsik/dorm/rooms/backed/controller/MainController.java
+++ src/main/java/hu/davidorcsik/dorm/rooms/backed/controller/MainController.java
@@ -1,19 +1,25 @@
 package hu.davidorcsik.dorm.rooms.backed.controller;
 
-import hu.davidorcsik.dorm.rooms.backed.entity.People;
-import hu.davidorcsik.dorm.rooms.backed.model.PeopleModel;
-import hu.davidorcsik.dorm.rooms.backed.model.RoomModel;
-import hu.davidorcsik.dorm.rooms.backed.security.UserWrapper;
-import hu.davidorcsik.dorm.rooms.backed.types.ReservationData;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.util.ArrayUtil;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.security.RolesAllowed;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 
 //TODO: this controller must not be part of the release build
 @RestController
@@ -24,8 +30,40 @@ public class MainController {
         return "home page";
     }
 
+    public static class ByteCollectorOutputStream extends OutputStream {
+        private final List<Byte> bytes = new ArrayList<>();
+        @Override
+        public void write(int b) { bytes.add((byte)b); }
+
+        public byte[] getBytes() { return (byte[]) ArrayUtils.toPrimitive(bytes.toArray(new Byte[0])); }
+    }
     @GetMapping("/test")
-    public People test() {
-        return ((UserWrapper)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getPeople();
+    public ResponseEntity test() throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet personSheet = workbook.createSheet("People");
+        Row headerRow = personSheet.createRow(0);
+
+        Cell cellHeaderName     =   headerRow.createCell(0);
+        Cell cellHeaderNeptun   =   headerRow.createCell(1);
+        Cell cellHeaderEmail    =   headerRow.createCell(2);
+        Cell cellHeaderNewbie   =   headerRow.createCell(3);
+        Cell cellHeaderSex      =   headerRow.createCell(4);
+        Cell cellHeaderRoom     =   headerRow.createCell(5);
+        Cell cellHeaderLabels   =   headerRow.createCell(6);
+
+        cellHeaderName.setCellValue("Name");
+        cellHeaderNeptun.setCellValue("Neptun");
+        cellHeaderEmail.setCellValue("Email");
+        cellHeaderNewbie.setCellValue("Newbie");
+        cellHeaderSex.setCellValue("Sex");
+        cellHeaderRoom.setCellValue("Room");
+        cellHeaderLabels.setCellValue("Labels");
+
+        ByteCollectorOutputStream os = new ByteCollectorOutputStream();
+        workbook.write(os);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=export.xlsx")
+                .body(os.getBytes());
     }
 }
