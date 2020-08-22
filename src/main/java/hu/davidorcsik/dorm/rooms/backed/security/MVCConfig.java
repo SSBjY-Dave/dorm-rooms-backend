d81c5fe3f1c2935b2d0b60c7685dcb714e8907fe
David Orcsik <davidorcsik@protonmail.com>
2020-08-22 22:47:05 +0200

    Removed MVC config for now

--- src/main/java/hu/davidorcsik/dorm/rooms/backed/security/MVCConfig.java
+++ src/main/java/hu/davidorcsik/dorm/rooms/backed/security/MVCConfig.java
@@ -5,17 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
-public class MVCConfig extends WebMvcConfigurationSupport {
-    private final UserAuthorityInterceptor userAuthorityInterceptor;
-
-    @Autowired
-    public MVCConfig(UserAuthorityInterceptor userAuthorityInterceptor) {
-        this.userAuthorityInterceptor = userAuthorityInterceptor;
-    }
-
-    @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(userAuthorityInterceptor);
-    }
-}
+//@Configuration
+//public class MVCConfig extends WebMvcConfigurationSupport {
+//    private final UserAuthorityInterceptor userAuthorityInterceptor;
+//
+//    @Autowired
+//    public MVCConfig(UserAuthorityInterceptor userAuthorityInterceptor) {
+//        this.userAuthorityInterceptor = userAuthorityInterceptor;
+//    }
+//
+//    @Override
+//    protected void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(userAuthorityInterceptor);
+//    }
+//}
